43e76fb5c9228e595e43317c8fb9ef8567cea9dd
huangyan321 <1604549268@qq.com>
2022-07-08 17:28:16 +0800

    add:新增公共组件自动注册

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import '@/styles/index.scss'; // global css
 import '@/styles/public.css'; // global css
 import 'normalize.css/normalize.css'; // a modern alternative to CSS resets
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/components';
 import elementUI from 'element-ui';
 Vue.config.productionTip = false;
 Vue.use(elementUI);
