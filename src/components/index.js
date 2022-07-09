95b0d6ca969574cee9258e1cfaa966ae76295c87
huangyan321 <1604549268@qq.com>
2022-07-09 21:04:41 +0800

    fix:err

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,14 @@
-// components/index.js
-import Vue from 'vue'
+// // components/index.js
+// import Vue from 'vue'
 
-// 自动加载 global 目录下的 .js 结尾的文件
-const componentsContext = require.context('./common', true, /\.js$/)
+// // 自动加载 global 目录下的 .js 结尾的文件
+// const componentsContext = require.context('./common', true, /\.js$/)
 
-componentsContext.keys().forEach(component => {
-  const componentConfig = componentsContext(component)
-  /**
-  * 兼容 import export 和 require module.export 两种规范
-  */
-  const ctrl = componentConfig.default || componentConfig
-  Vue.component(ctrl.name, ctrl)
-})
+// componentsContext.keys().forEach(component => {
+//   const componentConfig = componentsContext(component)
+//   /**
+//   * 兼容 import export 和 require module.export 两种规范
+//   */
+//   const ctrl = componentConfig.default || componentConfig
+//   Vue.component(ctrl.name, ctrl)
+// })
